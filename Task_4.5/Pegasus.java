f4daf2b9fec96d59ae9d837bc9966d1edbac364f
MistMax <88132030+MistMax@users.noreply.github.com>
2021-08-04 23:23:45 +0300

    Create Pegasus.java

---
+++ Task_4.5/Pegasus.java
@@ -0,0 +1,8 @@
+public class Pegasus extends Horse {
+    public Pegasus (boolean veg, String speak, byte paws) {
+        super(veg, speak, paws);
+    }
+    public void fly() {
+        System.out.println("Игого, я полетел(а)");
+    }
+}
