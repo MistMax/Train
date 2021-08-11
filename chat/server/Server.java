204e9ea6f9a861b12fab394f5c92ed0b9af2381e
MistMax <88132030+MistMax@users.noreply.github.com>
2021-08-11 08:22:07 +0300

    Update Server.java

--- chat/server/Server.java
+++ chat/server/Server.java
@@ -13,27 +13,30 @@ public class Server {
             try {
                 ServerSocket serverSocket = new ServerSocket(8188); // Создаём серверный сокет
                 System.out.println("Сервер запущен");
-                while (true){ // Бесконечный цикл для ожидания родключения клиентов
-                    Thread client = new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                Socket socket = serverSocket.accept(); // Ожидаем подключения клиента
-                                System.out.println("Клиент подключился");
-                                DataInputStream in = new DataInputStream(socket.getInputStream()); // Поток ввода
-                                DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // Поток вывода
-                                out.writeUTF("Добро пожаловать на сервер");
-                                while (true) {
-                                    String request = in.readUTF(); // Ждём сообщение от пользователя
-                                    out.writeUTF(request.toUpperCase()); // Отправляем сообщение пользователю
+                while (true) { // Бесконечный цикл для ожидания родключения клиентов
+                        try {
+                            Socket socket = serverSocket.accept(); // Ожидаем подключения клиента
+                            System.out.println("Клиент подключился");
+                            DataInputStream in = new DataInputStream(socket.getInputStream()); // Поток ввода
+                            DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // Поток вывода
+                            Thread client = new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+                                        out.writeUTF("Добро пожаловать на сервер");
+                                        while (true) {
+                                            String request = in.readUTF(); // Ждём сообщение от пользователя
+                                            out.writeUTF(request.toUpperCase()); // Отправляем сообщение пользователю
+                                        }
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
                                 }
-                            } catch (Exception e) {
-                            }
+                            });
+                            client.start();
+                        } catch (Exception e) {
                         }
-                    });
-                    client.start();
-
-                }
+                    }
             } catch (IOException e) {
                 e.printStackTrace();
             }
